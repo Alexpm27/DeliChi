38d9f5cec9d26e7a3abec6cbda6366d70d9b14d6
Alex <213523@ids.upchiapas.edu.mx>
2022-10-20 23:02:14 -0500

    creation of the CRUD Ceo

--- src/main/java/com/example/demorestaurant/services/CeoCerviceImpl.java
+++ src/main/java/com/example/demorestaurant/services/CeoCerviceImpl.java
@@ -1,4 +1,89 @@
 package com.example.demorestaurant.services;
 
-public class CeoCerviceImpl {
+import com.example.demorestaurant.controllers.dtos.request.CreateCeoRequest;
+import com.example.demorestaurant.controllers.dtos.request.UpdateCeoRequest;
+import com.example.demorestaurant.controllers.dtos.response.CreateCeoResponse;
+import com.example.demorestaurant.controllers.dtos.response.GetCeoResponse;
+import com.example.demorestaurant.controllers.dtos.response.UpdateCeoResponse;
+import com.example.demorestaurant.entities.Ceo;
+import com.example.demorestaurant.repositories.ICeoRepository;
+import com.example.demorestaurant.services.interfaces.ICeoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CeoCerviceImpl implements ICeoService {
+    @Autowired
+    private ICeoRepository repository;
+
+    @Override
+    public CreateCeoResponse create(CreateCeoRequest request) {
+        return from(repository.save(from(request)));
+    }
+
+    @Override
+    public GetCeoResponse get(Long id) {
+        return from_get(FindAndEnsureExist(id));
+    }
+
+    @Override
+    public UpdateCeoResponse update(UpdateCeoRequest request, Long id) {
+        Ceo ceo = FindAndEnsureExist(id);
+        ceo.setPhone_number(request.getPhone_number());
+        ceo.setEmail(request.getEmail());
+        ceo.setPassword(request.getPassword());
+        return from_upd(repository.save(ceo));
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(FindAndEnsureExist(id));
+    }
+
+    CreateCeoResponse from(Ceo ceo){
+        CreateCeoResponse response = new CreateCeoResponse();
+        response.setId(ceo.getId());
+        response.setName(ceo.getName());
+        response.setLast_name(ceo.getLast_name());
+        response.setPhone_number(ceo.getPhone_number());
+        response.setEmail(ceo.getEmail());
+        response.setPassword(ceo.getPassword());
+        return response;
+    }
+    Ceo from(CreateCeoRequest request){
+        Ceo ceo = new Ceo();
+        ceo.setName(request.getName());
+        ceo.setLast_name(request.getLast_name());
+        ceo.setPhone_number(request.getPhone_number());
+        ceo.setEmail(request.getEmail());
+        ceo.setPassword(request.getPassword());
+        return ceo;
+    }
+
+    GetCeoResponse from_get(Ceo ceo){
+        GetCeoResponse response = new GetCeoResponse();
+        response.setId(ceo.getId());
+        response.setName(ceo.getName());
+        response.setLast_name(ceo.getLast_name());
+        response.setPhone_number(ceo.getPhone_number());
+        response.setEmail(ceo.getEmail());
+        response.setPassword(ceo.getPassword());
+        return response;
+    }
+
+    UpdateCeoResponse from_upd(Ceo ceo){
+        UpdateCeoResponse response = new UpdateCeoResponse();
+        response.setId(ceo.getId());
+        response.setName(ceo.getName());
+        response.setLast_name(ceo.getLast_name());
+        response.setPhone_number(ceo.getPhone_number());
+        response.setEmail(ceo.getEmail());
+        response.setPassword(ceo.getPassword());
+        return response;
+    }
+
+    private Ceo FindAndEnsureExist(Long id){
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("not found"));
+    }
+
 }
--- src/main/java/com/example/demorestaurant/services/interfaces/ICeoService.java
+++ src/main/java/com/example/demorestaurant/services/interfaces/ICeoService.java
@@ -1,4 +1,17 @@
 package com.example.demorestaurant.services.interfaces;
 
+import com.example.demorestaurant.controllers.dtos.request.CreateCeoRequest;
+import com.example.demorestaurant.controllers.dtos.request.UpdateCeoRequest;
+import com.example.demorestaurant.controllers.dtos.response.CreateCeoResponse;
+import com.example.demorestaurant.controllers.dtos.response.GetCeoResponse;
+import com.example.demorestaurant.controllers.dtos.response.UpdateCeoResponse;
+
 public interface ICeoService {
+    CreateCeoResponse create(CreateCeoRequest request);
+
+    GetCeoResponse get(Long id);
+
+    UpdateCeoResponse update(UpdateCeoRequest request, Long id);
+
+    void delete(Long id);
 }
