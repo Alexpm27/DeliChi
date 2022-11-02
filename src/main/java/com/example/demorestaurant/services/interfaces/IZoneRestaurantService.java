db30b531991a969b41681d0bb48ef94c1bdc00bc
Alan <213376@ids.upchiapas.edu.mx>
2022-11-01 23:49:55 -0600

    delete of IZonaRestaurantRepository

--- src/main/java/com/example/demorestaurant/services/interfaces/IZoneRestaurantService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demorestaurant.services.interfaces;
-
-import com.example.demorestaurant.controllers.dtos.responses.BaseResponse;
-import com.example.demorestaurant.controllers.dtos.responses.RestaurantResponse;
-
-import java.util.List;
-
-public interface IZoneRestaurantService {
-}
